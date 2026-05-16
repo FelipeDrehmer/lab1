f40a91ae3d764a69ebc40ac05946da39d2bc03bb
Felipe Drehmer <felipeka.drehmer@gmail.com>
2026-05-15 21:48:32 -0300

    feat: adicionado testes

--- src/test/java/br/com/faculdadedonaduzzi/lab/LabApplicationTests.java
+++ src/test/java/br/com/faculdadedonaduzzi/lab/LabApplicationTests.java
@@ -1,13 +1,26 @@
 package br.com.faculdadedonaduzzi.lab;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LabApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void getAllTasks() throws Exception {
+		mockMvc.perform(get("/tarefa"))
+				.andExpect(status().isOk());
 	}
 
 }
